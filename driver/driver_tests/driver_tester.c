2417d334b5185a556daecb07791a7c2c9a61422c
Scott Plummer <Scott.Plummer18@gmail.com>
2018-07-24 10:55:34 -0700

    Add comments to clarify 2d array requirements

--- driver/driver_tests/driver_tester.c
+++ driver/driver_tests/driver_tester.c
@@ -15,6 +15,8 @@
 
 #define MATRIX_LENGTH 16
 #define MATRIX_HEIGHT 32
+//buff encodes the position (x,y) as a position in the array, and the value at the position is the 8-bit colour to display on the matrix. 
+//The array is roated 90 degrees from the physical matrix. 
 int buff[MATRIX_HEIGHT][MATRIX_LENGTH] = {
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -49,26 +51,29 @@ int buff[MATRIX_HEIGHT][MATRIX_LENGTH] = {
 	{0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-	int main(int argc, char **args){
-		FILE *fd = fopen(args[1], "w");
-if (!fd)
+int main(int argc, char **args)
 {
-	fprintf(stderr, "Opening file %s failed with error %s!\n", args[1], strerror(errno));
-	return 1;
-}
-// while (1) {
+	FILE *fd = fopen(args[1], "w");
+	if (!fd)
+	{
+		fprintf(stderr, "Opening file %s failed with error %s!\n", args[1], strerror(errno));
+		return 1;
+	}
 
-// }
-int bytes_written = fwrite(buff, sizeof(int), MATRIX_LENGTH * MATRIX_HEIGHT, fd);
-if (bytes_written != MATRIX_LENGTH * MATRIX_HEIGHT)
-{
-	fprintf(stderr, "Writing to file %s failed with error %s!\n", args[1], strerror(errno));
-}
-else
-{
-	printf("Wrote to file [%s] %d number of bytes!\n", args[1], bytes_written);
-}
+	while (1)
+	{
+		int bytes_written = fwrite(buff, sizeof(int), MATRIX_LENGTH * MATRIX_HEIGHT, fd);
+		if (bytes_written != MATRIX_LENGTH * MATRIX_HEIGHT)
+		{
+			fprintf(stderr, "Writing to file %s failed with error %s!\n", args[1], strerror(errno));
+		}
+		else
+		{
+			printf("Wrote to file [%s] %d number of bytes!\n", args[1], bytes_written);
+		}
+		sleep(1);
+	}
 
-fclose(fd);
-return 0;
+	fclose(fd);
+	return 0;
 }
